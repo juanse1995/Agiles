668096af329378e3a27a42d199de0393889a34d8
jovannyro <ne.rodriguez@javeriana.edu.co>
2021-09-22 21:51:00 -0500

    master

--- EMPRESA/Empresa.App/Dominio/Entidades/Empresa.cs
+++
@@ -1,9 +0,0 @@
-namespace Dominio
-{
-    public class Empresa
-    {
-        public int Id { get; set; }
-        public string Nombre { get; set; }
-        public string CIF { get; set; }
-    }
-}
