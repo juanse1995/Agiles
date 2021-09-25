2a35c3bf081c48346f0f72285605cf62d445be18
juanse1995 <juansebastian_512@hotmail.com>
2021-09-25 13:25:54 -0500

    Master

--- Empresa.App/FrontEnd/Startup.cs
+++ Empresa.App/FrontEnd/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Persistencia;
+
 
 
 namespace FrontEnd
@@ -21,14 +21,19 @@ namespace FrontEnd
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration) 
+        {
+            this.Configuration = configuration;
+               
+        }
+                public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
             Services.AddSingleton<I_RepositorioPersona,RepositorioPersona>();
-             
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
