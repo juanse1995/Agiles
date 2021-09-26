18c552cc3cc86499d2fee5aa04834f095027f228
jovannyro <ne.rodriguez@javeriana.edu.co>
2021-09-26 12:42:57 -0500

    master

---
+++ Empresa.App/FrontEnd/Pages/Empresas/List.cshtml.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Dominio;
+using Persistencia;
+
+namespace Frontend.Pages
+{
+    public class ListModelEmpresas : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
