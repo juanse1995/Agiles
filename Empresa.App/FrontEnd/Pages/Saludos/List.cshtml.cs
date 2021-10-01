35ecc09080c4130c430be8e95f9d18135bc197da
jovannyro <ne.rodriguez@javeriana.edu.co>
2021-09-30 19:17:19 -0500

    master

--- Empresa.App/FrontEnd/Pages/Saludos/List.cshtml.cs
+++ Empresa.App/FrontEnd/Pages/Saludos/List.cshtml.cs
@@ -12,28 +12,18 @@ namespace Frontend.Pages
 {
     public class ListModel : PageModel
     {
-        //private string[] saludos = { "Buenos dias", "Buenas tardes", "Buenas noches", "Hasta mañana" };
-        //public List<string> ListaSaludos { get; set; }
 
         private readonly I_RepositorioSaludos repositorioSaludos;
         public IEnumerable<Saludo> Saludos {get; set;}
+
         public ListModel(I_RepositorioSaludos repositorioSaludos)
         {
             this.repositorioSaludos = repositorioSaludos;
         }
 
-        //private readonly I_RepositorioPersona repositorioPersona;
-        //private readonly I_RepositorioEmpresa repositorioEmpresa;
-        //private readonly I_RepositorioCliente repositorioCliente;
-        //private readonly I_RepositorioEmpleado repositorioEmpleado;
-        //private readonly I_RepositorioDirectivo repositorioDirectivo;
-
         public void OnGet()
-        {
-            //ListaSaludos = new List<string>();
-            //ListaSaludos.AddRange(saludos);
+        {           
             Saludos = repositorioSaludos.GetAll();
         }
-
     }
 }
