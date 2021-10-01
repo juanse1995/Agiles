5e518c85d9576814d3bdd08d402710bbd723d06b
jovannyro <ne.rodriguez@javeriana.edu.co>
2021-10-01 08:28:57 -0500

    aster

--- Empresa.App/FrontEnd/Startup.cs
+++ Empresa.App/FrontEnd/Startup.cs
@@ -28,9 +28,10 @@ namespace Frontend
         {
             services.AddRazorPages();
             
-            services.AddSingleton<I_RepositorioSaludos, RepositorioSaludos>(); 
-                        
-            //services.AddSingleton<I_RepositorioPersona, RepositorioPersona>();
+            
+            //services.AddSingleton<I_RepositorioSaludos, RepositorioSaludos>(); 
+            //services.AddTrasient<I_RepositorioPersona, RepositorioPersona>();           
+            services.AddScoped<I_RepositorioPersona, RepositorioPersona>();
             //services.AddSingleton<I_RepositorioEmpresa, RepositorioEmpresa>();
             //services.AddSingleton<I_RepositorioCliente, RepositorioCliente>();
             //services.AddSingleton<I_RepositorioEmpleado, RepositorioEmpleado>();
